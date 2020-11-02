200c29d885047093e77de949a16eedd39f6765b7
luisco28 <luisco28@gmail.com>
2020-11-02 09:13:01 -0800

    Error handling examples

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,75 @@
+use std::fs;
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    // version 2
+    let f2 = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+// Propagating errors
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// the ? operator (not even close to C's)
+fn read_user_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+// The ? operator evaluates an expression that returns Result, if Tesult id Ok
+// it assigns the OK, if it returns Err, the function returns the error to the caller
+// Here is an even shorter version of the function
+fn read_user_file_2() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+// taking away all the fun
+fn read_user_file_3() -> Result<String, io::Error> {
+    // read_to_string can create a new String a return it directly
+    fs::read_to_string("hello.txt")
 }
