0970b1f5f8e304cb41866c02997471e8102c1891
luisco28 <luisco28@gmail.com>
2021-02-17 13:13:36 -0700

    adding guessing game

---
+++ generics/src/main.rs
@@ -0,0 +1,72 @@
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = list[0];
+
+    for item in list {
+        if item > largest { 
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// struct generics 
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point2<T, U> {
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x, 
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest numbe is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    let both_integer = Point2 { x: 5, y: 10};
+    let both_float = Point2 { x: 1.0, y: 4.0 };
+    let integer_and_float = Point2 { x: 5, y: 4.0 };
+
+    println!("integer.x = {}", integer.x());
+
+    let distance = float.distance_from_origin();
+    println!("float is {} from the origin", distance);
+
+    let p1 = Point2 { x:5, y: 10.5 };
+    let p2 = Point2 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
